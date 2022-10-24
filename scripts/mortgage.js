2448989905ec5b10765d82ad3ffd9a2772fdfe92
alinakh1 <akhabibullina@nyc.yearup.org>
2022-10-24 13:03:05 -0400

    Final

--- scripts/mortgage.js
+++ scripts/mortgage.js
@@ -15,8 +15,8 @@ function calculateButtonOnClick()
     const length = document.getElementById("number3Field");
 
     let num1AsString = number1Field.value;
-    let num2AsString = number2Field.value;
-    let num3AsString = number3Field.value;
+    let num2AsSrting = number2Field.value;
+    let num3AsString = number3Feld.value;
 
     let principalLoan = Number(num1AsString);
     let interestRatePercent = Number(num2AsString);
